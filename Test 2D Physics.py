e0fe90d6164d3a1325ff82104e05fc64c38a522a
AmiRadoM <amirrado10@gmail.com>
2021-01-28 00:02:39 +0200

    -Converted Agine to a Python Module

--- Test 2D Physics.py
+++ Test 2D Physics.py
@@ -1,4 +1,4 @@
-from Agine_main import *
+from Agine import *
 import threading
 
 def update():
@@ -24,31 +24,33 @@ def start():
 
 
     ground.addAttr("BoxCollider")
-
+    #
     s1.addAttr("BoxCollider")
-    s2.addAttr("BoxCollider")
-
-
+    # s2.addAttr("BoxCollider")
+    #
+    #
     s1.BoxCollider.localScale.x = -25
-
-
-
+    #
+    #
+    #
     s1.addAttr("Rigidbody2D")
-    s2.addAttr("Rigidbody2D")
-
-
+    # s2.addAttr("Rigidbody2D")
 
     pass
 
 #Variables
 
-ground = Square(position= Vector2D(-100,-200), scale = Vector2D(500,100))
+camera = object2D().addAttr("Camera")
 
+ground = Square(position= Vector2D(-0,-0), scale = Vector2D(100,100))
+
+l1 = Line(endPoint=Vector2D(0,100))
 
 s1 = Sprite(image= "Character.png",name= "controlled",position=Vector2D(-100,200), scale= Vector2D(50,50))
-s2 = Square(name= "notControlled",position=Vector2D(-50,200), scale= Vector2D(50,50))
+# s2 = Square(name= "notControlled",position=Vector2D(-50,200), scale= Vector2D(50,50))
 # s3 = Square(name= "controlled",position=Vector2D(-0,-100), scale= Vector2D(50,50))
 # s4 = Square(name= "notControlled",position= Vector2D(51,-100), scale = Vector2D(50,50))
+
 
 start()
 
